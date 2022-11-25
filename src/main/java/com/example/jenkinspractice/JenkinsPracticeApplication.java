b68f5a2781b606fff3ee352580dccc0388748117
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-25 12:04:45 +0900

    Initial commit

---
+++ src/main/java/com/example/jenkinspractice/JenkinsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkinspractice/JenkinsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
